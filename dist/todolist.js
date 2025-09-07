f1b576562c01cb1dfab54c4e86631acac5add61b
Kaveh Khorshidi <kavehkhorshidiii@gmail.com>
2025-09-07 14:19:01 +0330

    delete todo

--- dist/todolist.js
+++ dist/todolist.js
@@ -2,21 +2,33 @@ const todoInput = document.querySelector(".todo-value");
 const addTodoBtn = document.querySelector(".add-todo");
 const clearTodos = document.querySelector(".clear-todos");
 const todoList = document.querySelector(".todoList");
-// todo list
+const iconBin = document.querySelector("icon");
+// todo list 
 const ToDo = [];
+// add new todo (Function)
 const addTodoBtnFunction = () => {
     const NewToDo = { id: ToDo.length + 1, text: todoInput.value };
     if (NewToDo.text !== "") {
         ToDo.push(NewToDo);
     }
-    todoList.innerHTML = ToDo.map(thisTodo => `<li class="todoList">
-        ${thisTodo.text}
-        <span class="icon"><i class="fas fa-trash"></i></span>
+    todoList.innerHTML = ToDo.map(thisToDo => `<li class="todoList">
+            ${thisToDo.text}
+            <span onclick="deleteToDo(${thisToDo.id})" class="icon"><i class="fas fa-trash"></i></span>
         </li>`).join('');
     todoInput.value = "";
     todoInput.focus();
 };
+// add new todo (Event)
 addTodoBtn.addEventListener("click", addTodoBtnFunction);
+// delete todo
+const deleteToDo = (ToDoId) => {
+    const filterToDo = ToDo.filter((item) => {
+        return item.id !== ToDoId;
+    });
+    ToDo.length = 0;
+    ToDo.push(...filterToDo);
+    addTodoBtnFunction();
+};
 // clear all ToDos
 clearTodos.addEventListener("click", () => {
     ToDo.length = 0;
--- src/todolist.ts
+++ src/todolist.ts
@@ -2,27 +2,28 @@ const todoInput = document.querySelector(".todo-value") as HTMLInputElement
 const addTodoBtn = document.querySelector(".add-todo") as HTMLButtonElement
 const clearTodos = document.querySelector(".clear-todos") as HTMLButtonElement
 const todoList = document.querySelector(".todoList") as HTMLUListElement
+const iconBin = document.querySelector("icon") as HTMLButtonElement
 
 
 // interface
-interface ToDoType { 
+interface ToDoType {
     id: number;
     text: string;
 }
 
-// todo list
+// todo list 
 const ToDo: ToDoType[] = []
 
-
+// add new todo (Function)
 const addTodoBtnFunction = () => {
-    const NewToDo:ToDoType = {id: ToDo.length + 1  ,  text:todoInput.value}
+    const NewToDo: ToDoType = { id: ToDo.length + 1, text: todoInput.value }
 
-    if(NewToDo.text !== ""){ ToDo.push(NewToDo) }
-    
-    todoList.innerHTML = ToDo.map( thisTodo =>
+    if (NewToDo.text !== "") { ToDo.push(NewToDo) }
+
+    todoList.innerHTML = ToDo.map(thisToDo =>
         `<li class="todoList">
-        ${thisTodo.text}
-        <span class="icon"><i class="fas fa-trash"></i></span>
+            ${thisToDo.text}
+            <span onclick="deleteToDo(${thisToDo.id})" class="icon"><i class="fas fa-trash"></i></span>
         </li>`
     ).join('')
 
@@ -30,12 +31,23 @@ const addTodoBtnFunction = () => {
     todoInput.focus()
 }
 
-
+// add new todo (Event)
 addTodoBtn.addEventListener("click", addTodoBtnFunction)
 
-    
+// delete todo
+const deleteToDo = (ToDoId: number) => {
+
+    const filterToDo = ToDo.filter((item:ToDoType) => {
+        return item.id !== ToDoId   
+    })
+
+    ToDo.length = 0
+    ToDo.push(...filterToDo)
+    addTodoBtnFunction()
+}
+
 // clear all ToDos
-clearTodos.addEventListener("click" , () => {
+clearTodos.addEventListener("click", () => {
     ToDo.length = 0
     addTodoBtnFunction()
 })
